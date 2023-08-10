fdb7d73ce105c700ae2dad9a5727f1a19f9548a4
theblackfogs <theblackfogs@gmail.com>
2023-08-09 22:58:06 -0700

    add c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /*
  * int main - return 0 value
+ * description - prints the string value and 
+ * 		 return 0
  * void - the function does not take any inputs
  * retun 0
  */
@@ -9,6 +11,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+
+	return (0);
 }
