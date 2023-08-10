43cd44e351291d065ff005f65be0db291140bb7f
theblackfogs <theblackfogs@gmail.com>
2023-08-09 22:55:29 -0700

    add first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * int main - return 0 value
+ * void - the function does not take any inputs
+ * retun 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
