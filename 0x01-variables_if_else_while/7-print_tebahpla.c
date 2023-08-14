bbc0ba25c6df67a2ea21315d2f2341fa9ad3145e
theblackfogs <theblackfogs@gmail.com>
2023-08-14 05:40:51 -0700

    add files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print some text to the screen
+ * @void: no parameters
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
