79c8ff2d70d67a3f4a88d408d0e7c2920f78a332
theblackfogs <theblackfogs@gmail.com>
2023-08-15 06:35:43 -0700

    add _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print some text to the screen
+ * @void: no parameters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (1);
+}
