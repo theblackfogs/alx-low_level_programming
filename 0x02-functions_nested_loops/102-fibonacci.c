1c5ffb703775c9cbaf24bcc54917db2e6a001912
theblackfogs <theblackfogs@gmail.com>
2023-08-16 14:51:21 -0700

    add files

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Entry point
+ *
+ * Description: main function
+ * @void: no parameter
+ *
+ *
+ * Return: 0 (Success)
+ */
+
+
+void fibonacci1(void);
+
+int main(void)
+{
+	fibonacci1();
+
+	return (0);
+}
+
+/**
+ *fibonacci1 - the first of 50 fibonacci
+ *
+ *Description: print some of fibonacci
+ *@void: no paramters
+ *
+ *Return: nothing
+ */
+
+
+void fibonacci1(void)
+{
+	int i;
+	long int first, second, total;
+
+	first = 1;
+	second = 2;
+
+	printf("%d, %d, ", 1, 2);
+
+	for (i = 3; i < 48; i++)
+	{
+		total = first + second;
+		printf("%ld, ", total);
+
+		first = second;
+		second = total;
+	}
+	total += first;
+	printf("%ld\n", total);
+
+}
