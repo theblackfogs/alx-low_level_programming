82e892f4d0d076cd2e235b09787e0155e8520e1e
theblackfogs <theblackfogs@gmail.com>
2023-08-17 02:02:50 -0700

    add files

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+void multiples_1024(void);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char);
+
+#endif /*MAIN_H_*/
