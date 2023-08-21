071fbcf769d60b6d622fb70fda895cd9e80c1ea6
theblackfogs <theblackfogs@gmail.com>
2023-08-21 16:38:53 -0700

    add files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_puts - function
+ *
+ * Description: print string to the screen
+ * @str:pointer of char parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		printf("%c", *(str + i));
+		i++;
+	}
+	if (i < 1)
+		putchar('0');
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 #include <stdio.h>
 
+void _puts(char *str);
 int _strlen(char *s);
 void print_array(int *a, int n);
 void swap_int(int *a, int *b);
