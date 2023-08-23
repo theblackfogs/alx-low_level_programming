ec525967bafc9029396c5640932ad6bf76678917
theblackfogs <theblackfogs@gmail.com>
2023-08-23 15:54:39 -0700

    add files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_strcat - function
+ *
+ * Description: combines two strings
+ * @dest:pointer of char parameter
+ * @src:pointer of char parameter
+ *
+ * Return: pointer of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, last;
+	int length2, limit, x;
+
+	length2 = sizeof(src);
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+		last = i;
+
+	limit = last + length2;
+
+	for (j = last + 1; j < limit; j++)
+	{
+		*(dest + j) = *(src + x);
+		x++;
+	}
+	*(dest + limit) = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src);
+
+
+#endif /*MAIN_H_*/
