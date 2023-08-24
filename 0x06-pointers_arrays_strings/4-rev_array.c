c1fee6288f3552d358c635aa124923e09549d311
theblackfogs <theblackfogs@gmail.com>
2023-08-24 03:51:46 -0700

    add reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *reverse_array - function
+ *
+ * Description: print reversed array
+ * @a:pointer of char parameter
+ * @n:integer  parameter
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
