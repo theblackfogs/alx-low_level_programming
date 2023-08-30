5008e8ab28eddef1cf1baff1e1616aeae2e8cf1c
theblackfogs <theblackfogs@gmail.com>
2023-08-30 15:25:04 -0700

    add palindrome strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,14 +5,5 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *rot13(char *);
-char *leet(char *);
-char *cap_string(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-
+int is_palindrome(char *s);
 #endif /*MAIN_H_*/
