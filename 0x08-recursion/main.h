899d2f5f10327e25d17e66d80909fd26aa4810cc
theblackfogs <theblackfogs@gmail.com>
2023-08-30 15:15:08 -0700

    add files

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+#endif /*MAIN_H_*/
