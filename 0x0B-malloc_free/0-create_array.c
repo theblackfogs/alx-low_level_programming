6d6a5f34b825fede471039871cc0cff80b96700b
theblackfogs <theblackfogs@gmail.com>
2023-09-04 13:04:06 -0700

    add file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *create_array - function
+ *
+ * Description: create an array
+ * @size: integer
+ * @c: char
+ *
+ * Return: pointer of dest
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	str = (char *)malloc(size * sizeof(char) + 1);
+
+	if (size == 0 || str == NULL)
+		return (NULL);
+
+	*str = c;
+	return (str);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif 
