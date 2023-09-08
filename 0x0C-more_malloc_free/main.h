4e7ba7b42db8e971f45df5159e15115063bb3c5e
theblackfogs <theblackfogs@gmail.com>
2023-09-08 02:45:54 -0700

    add conca

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,4 +10,7 @@ void usage(void);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+
 #endif
