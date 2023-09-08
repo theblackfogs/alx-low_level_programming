1e771ff90a335daf60a45dd1c0cacb8d0d486d60
theblackfogs <theblackfogs@gmail.com>
2023-09-08 03:15:34 -0700

    add files

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,19 +15,17 @@
 int *array_range(int min, int max)
 {
 
-	int *ptr;
-	int size, i;
+	int *ptr, size, i;
 
 	if (min > max)
 		return (NULL);
 
-	size = max - min;
-	ptr = (int *) malloc(sizeof(int) * size + 1);
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = min++;
