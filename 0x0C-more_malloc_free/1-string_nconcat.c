20ec0840ecb71b01ec8a4322db539124a7728c9d
theblackfogs <theblackfogs@gmail.com>
2023-09-08 02:44:48 -0700

    add conc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ *string_nconcat - function
+ *
+ *Description: join two strings together
+ *
+ *@s1: char parameter
+ *@s2: char parameter
+ *@n: integer parameter
+ *
+ *Return: NULL or pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	char *s;
+	unsigned int len = n, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+		return (NULL);
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		s[len++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		s[len++] = s2[i];
+
+	s[len] = '\0';
+
+	return (s);
+}
