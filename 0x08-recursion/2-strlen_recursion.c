fb7b36c5da875ced1325d120dd4c1a3d73210b7d
theblackfogs <theblackfogs@gmail.com>
2023-10-26 12:41:04 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *int _strlen_recursion - function
+ *
+ * Description: print out string
+ * @s: pointer parameter
+ *
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + int _strlen_recursion(++s));
+}
