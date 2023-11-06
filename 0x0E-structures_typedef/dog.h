1de2bde03fe8b4406835c2905c47b6f412092019
theblackfogs <theblackfogs@gmail.com>
2023-11-06 11:49:55 -0800

    add c structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+
+/**
+ * struct dog - Dog details
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Dog structure
+ */
+
 struct dog
 {
 	char *name;
