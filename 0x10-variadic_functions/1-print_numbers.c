05f157b64da57c6ead5d08224df8f7105bff6e7c
theblackfogs <theblackfogs@gmail.com>
2023-11-10 03:09:17 -0800

    add files

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n != 0)
+	if (n <= 0)
 	{
 		va_start(args, n);
 		if (separator != NULL)
